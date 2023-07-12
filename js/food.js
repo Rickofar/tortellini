109e10711774b0feb0e59fac0cc1db42c100def2
Frederick Adarode <109428219+Rickofar@users.noreply.github.com>
2023-07-12 21:20:49 +0100

    Create food.js

---
+++ js/food.js
@@ -0,0 +1,83 @@
+// breakfast
+const breakfastLists = [
+	{
+		image: "img/food/breakfast/toast-egg-bacon-tomatoes-microgreens-salad.jpg",
+		name: "Toast Bread",
+		category: "Breakfast",
+		price: 350000,
+	},
+	{
+		image:
+			"img/food/breakfast/english-breakfast-fried-eggs-sausages-zucchini-sweet-peppers.jpg",
+		name: "English Breakfast Fried Eggs & Sausages (with Zucchini Sweet Peppers)",
+		category: "Breakfast",
+		price: 450000,
+	},
+	{
+		image: "img/food/breakfast/1.JPG",
+		category: "Breakfast",
+		name: "Prawn & Avocado Salad",
+		price: 300000,
+	},
+];
+
+//lunch
+const lunchLists = [
+	{
+		image:
+			"img/food/lunch/front-view-vegetable-chicken-salad-inside-plate-grey-table-health-salad-diet-food.jpg",
+		name: "Vegetable Chicken Salad",
+		category: "Lunch",
+		price: 400000,
+	},
+	{
+		image:
+			"img/food/lunch/spaghetti-with-herbs-vegetables-white-plate-top-view.jpg",
+		name: "Spaghetti with Herbs Vegetables",
+		category: "Lunch",
+		price: 500000,
+	},
+
+	{
+		image: "img/food/lunch/2.JPG",
+		category: "Lunch",
+		name: "Spaghetti with Mixed Vegetable",
+		price: 420000,
+	},
+];
+
+// dinner
+
+const dinnerLists = [
+	{
+		image:
+			"img/food/dinner/pasta-spaghetti-with-shrimps-tomato-sauce-served-plate-dark-surface-closeup.jpg",
+		category: "Dinner",
+		name: "Pasta Spaghetti with Shrimps Tomato Sauce",
+		price: 500000,
+	},
+];
+
+// today's menu list
+
+const todaysMenuLists = [
+	{
+		image: "img/food/breakfast/1.JPG",
+		category: "Breakfast",
+		name: "Prawn & Avocado Salad",
+		price: 300000,
+	},
+	{
+		image: "img/food/lunch/2.JPG",
+		category: "Lunch",
+		name: "Spaghetti with Mixed Vegetable",
+		price: 420000,
+	},
+	{
+		image:
+			"img/food/dinner/pasta-spaghetti-with-shrimps-tomato-sauce-served-plate-dark-surface-closeup.jpg",
+		category: "Dinner",
+		name: "Pasta Spaghetti with Shrimps Tomato Sauce",
+		price: 500000,
+	},
+];
